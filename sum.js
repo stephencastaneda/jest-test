46e6921896affc9fa83dde006ea8ff84e93a40bf
Stephen Castaneda <stephencastaneda@Stephens-MacBook-Pro.local>
2022-05-27 10:42:06 -0700

    finished exercise

--- sum.js
+++ sum.js
@@ -3,22 +3,35 @@ const isString = (s) => typeof s === 'string'
 const isBoolean = (b) => typeof b === 'boolean'
 const isObject = (o) => typeof o === 'object'
 
+
 function sum(a, b) {
   if (arguments.length < 2) {
-   throw new Error('You must pass two numbers')
+   throw new Error('You must use two numbers!')
   } else if (isString(a) || isString(b)) {
-     throw new Error('Give me a number! This is a string!')
+     throw new Error('You must use a number!')
    } else if (isBoolean(a) || isBoolean(b)) {
-     throw new Error('Give me a number! This is a boolean!')
+     throw new Error('You must use a number!')
    } else if (isObject(a) || isObject(b)){
-     throw new Error('Give me a number! This is an object!')
+     throw new Error('You must use a number!')
+   } else if (isNaN(a) || isNaN(b)) {
+     throw new Error('You must use a number!')
    } else if (isNumber(a) || isNumber(b)) {
      return a + b
    } else {
      return 0
    }
  }
- 
 
-module.exports = sum
+ 
+function sumArray(numbers) {
+  if (numbers.every(element => typeof element === 'number')) {
+      const reducer = (acc, val) => { return acc + val }
+      const initialValue = 0
+      return numbers.reduce(reducer, initialValue)
+    } else {
+      throw new Error ("You must only use numbers!")
+    } 
+}
+
+module.exports = { sum, sumArray }
 
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+const { sum, sumArray } = require('./sum');
 
 describe("sum function", () => {
 
@@ -7,19 +7,65 @@ describe("sum function", () => {
   })
 
   it("if a string is passed, an error is thrown", () => {
-    expect(() => sum(1, 'test')).toThrowError('Give me a number! This is a string!')
+    expect(() => sum(1, 'test')).toThrowError('You must use a number!')
   })
 
   it("if a boolean is passed, an error is thrown", () => {
-    expect(() => sum(1, true)).toThrowError('Give me a number! This is a boolean!')
+    expect(() => sum(1, true)).toThrowError('You must use a number!')
   })
 
   it("if an object is passed, an error is thrown", () => {
-    expect(() => sum({}, 2)).toThrowError('Give me a number! This is an object!')
+    expect(() => sum({}, 2)).toThrowError('You must use a number!')
   })
 
   it("must pass at least two numbers", () => {
-    expect(() => sum(1)).toThrowError('You must pass two numbers')
+    expect(() => sum(1)).toThrowError('You must use two numbers!')
+  })
+});
+
+// 2. Instead of listing the inverses every time, just have two exceptions running through the sum check, and do (a,b) and then (b,a)
+
+describe("sum exception test", () => {
+  it.each([
+    [true, 0, 'You must use a number!'],
+    [{}, -2, 'You must use a number!'],
+    ['string', 0, 'You must use a number!'],
+    [[], 0, 'You must use a number!'],
+    [() => {}, () => {}, 'You must use a number!'],
+  ])(
+    `should return proper result when passed arguments are: %i, %i`,
+    (a, b, result) => {
+      expect(() => sum(a, b)).toThrowError(result);
+      expect(() => sum(b, a)).toThrowError(result);
+    }
+  );
+});
+
+describe("array function", () => {
+  it("returns integers in an array as a sum", () => {
+    expect(sumArray([2, 3, 5, 10])).toBe(20)
   })
 
+  it("throw error if non-number in array", () => {
+    expect(() => sumArray([2, 'true', 5])).toThrowError("You must only use numbers!")
+  })
+
+})
+
+describe("sum array exception test", () => {
+  it.each([
+    [5, 0, true, 'You must only use numbers!'],
+    [{}, -5, 6, 'You must only use numbers!'],
+    ['string', 7, 9, 'You must only use numbers!'],
+    [[], 15, 14, 'You must only use numbers!'],
+    [() => {}, () => {}, 5, 'You must only use numbers!'],
+  ])(
+    `should return proper result when passed arguments are: %i, %i, %i`,
+    (a, b, c, result) => {
+      expect(() => sumArray([a, b, c])).toThrowError(result);
+      expect(() => sumArray([b, a, c])).toThrowError(result);
+      expect(() => sumArray([c, a, b])).toThrowError(result);
+    }
+  );
 });
+
